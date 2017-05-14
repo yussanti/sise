b20c9cf8db739476b87f003aced9c24f1ebe8fd3
yussanti nur fajrina <yussantinf@gmail.com>
2017-05-14 16:36:45 +0700

    fixing some lect

--- seeCoursesLect.php
+++ seeCoursesLect.php
@@ -5,14 +5,13 @@ include 'inc/connection.php'
 <?php
 //cek apakah user sudah login
 if(!isset($_SESSION['username'])){
-    die("index.php");
+    header("Location: index.php");//
 }
 
 //cek level user
 if($_SESSION['level']!="Dosen"){
-    header("index.php");
-}
-?>
+    header("Location: index.php");
+}?>
 <?php include 'inc/configDosen.php'; $template['header_link'] = 'WELCOME'; ?>
 <?php include 'inc/template_start.php'; ?>
 <?php include 'inc/page_head.php'; ?>
@@ -27,7 +26,7 @@ if($_SESSION['level']!="Dosen"){
     <!-- Datatables is initialized in js/pages/uiTables.js -->
     <div class="block full">
         <div class="block-title">
-            <h2>My Courses</h2>
+            <h2>View Courses</h2>
         </div>
         <div class="table-responsive">
             <table id="example-datatable" class="table table-striped table-bordered table-vcenter">
@@ -35,27 +34,27 @@ if($_SESSION['level']!="Dosen"){
                     <tr>
                         <th style="width: 120px;" class="text-center">ID Courses</th>
                         <th class="text-center">Courses</th>
-                        <th class="text-center">Lecturer</th>
-                    
+                        <th class="text-center" style="width: 55;">Participants</i></th>
+
+                        <th class="text-center" style="width: 55;">Exams</i></th>
+
                     </tr>
                 </thead>
                 <tbody>
                    <tr>
-
-                        <?php 
-                    $name = $_SESSION['username']; 
-                    $query1 = mysql_query("SELECT * FROM tb_courses WHERE username ='$name'");
+                        <?php    
+                        $username=$_SESSION['username'];
+                        $query1 = mysql_query("SELECT * FROM tb_courses WHERE username='$username'");
     
-                    $jumlah = mysql_num_rows($query1);
+                     $jumlah = mysql_num_rows($query1);
                     while($baris1=mysql_fetch_array($query1)) {
                     ?> 
                     <tr>
                         <td><center><?php echo $baris1['idmk'] ;?></center></td>
                         
                         <td><center><?php echo $baris1['courses'] ;?></center></td>
-                        <td><center><?php echo $baris1['username'] ;?></center></td>
-                        
-                
+                        <td><center><a href="seeParticipantLect.php?idmk=<?php echo $baris1['idmk']; ?>"><button type="submit" class="btn btn-effect-ripple btn-primary" name="submit" id="submit">View</button></a></center></td>
+                        <td><center><a href="seeExam.php?idmk=<?php echo $baris1['idmk']; ?>"><button type="submit" class="btn btn-effect-ripple btn-primary" name="submit" id="submit">View</button></a></center></td>
                     </tr>
                     <?php } ?>
                    
--- seeGradesLect.php
+++ seeGradesLect.php
@@ -26,52 +26,48 @@ if($_SESSION['level']!="Dosen"){
     <!-- Datatables is initialized in js/pages/uiTables.js -->
     <div class="block full">
         <div class="block-title">
-            <h2>Examination</h2>
+            <h2>View Courses</h2>
         </div>
         <div class="table-responsive">
             <table id="example-datatable" class="table table-striped table-bordered table-vcenter">
                 <thead>
                     <tr>
                         <th style="width: 120px;" class="text-center">ID Courses</th>
-                        <th class="text-center">Exam Name</th>
-                        <th class="text-center">Password</th>
-                        <th class="text-center">Duration</th>
-                        <th class="text-center">Status</th>
-                        <th class="text-center" style="width: 75px;">Details</th>
+                        <th class="text-center">Courses</th>
+                        <th class="text-center">Participant</th>
+                        
+                        <th class="text-center" style="width: 100px;"><i class="gi gi-cogwheel"></i></th>
+
                     </tr>
                 </thead>
                 <tbody>
                    <tr>
 
-                        <?php 
-
-                        if (isset($idmk)) {
-                            $idmk = $_GET['idmk'];
-                        } else {
-                            $idmk="";
-                        }
-                    $username=$_SESSION['username'];
-                    $query1 = mysql_query("SELECT * FROM tb_exam where username='$username' AND idmk='$idmk'");
-                   
-                    $jumlah = mysql_num_rows($query1);
+                        <?php    
+                        $idmk=$_GET['idmk'];
+                        $username=$_SESSION['username'];
+                        $query1 = mysql_query("SELECT tb_peserta.idmk, tb_courses.courses, tb_peserta.username FROM tb_peserta INNER JOIN tb_courses ON tb_peserta.idmk=tb_peserta.idmk WHERE tb_peserta.idmk='$idmk' AND tb_courses.username='$username'");
+    
+                     $jumlah = mysql_num_rows($query1);
                     while($baris1=mysql_fetch_array($query1)) {
                     ?> 
                     <tr>
-                        <td><center><?php echo $baris1['idmk'] ;?></center></td>     
-                        <td><center><?php echo $baris1['exam'] ;?></center></td>
-                        <td><center><?php echo $baris1['passexam'] ;?></center></td>
-                        <td><center><?php echo $baris1['duration'] ;?></center></td>
-                        <td><center><?php echo $baris1['status'] ;?></center></td>
+                        <td><center><?php echo $baris1['idmk'] ;?></center></td>
                         
+                        <td><center><?php echo $baris1['courses'] ;?></center></td>
+                        <td><center><?php echo $baris1['username'] ;?></center></td>
                         <td class="text-center">
-                           <a href="addExamQ.php?idmk=<?php echo $baris1['idmk'];?>&idtest=<?php echo $baris1['idtest'];?>" data-toggle="tooltip" title="Edit Courses" class="btn btn-effect-ripple btn-xs btn-success"><i class="fa fa-pencil"></i></a>
+                            <a href="addParticipant.php?idmk=<?php echo $idmk; ?>" data-toggle="tooltip" title="add another participant" class="btn btn-effect-ripple btn-xs btn-info"><i class="gi gi-user_add"></i></a>
+                            <a href="deleteParticipant.php?usr=<?php echo $baris1['username']; ?>&idmk=<?php echo $idmk;?>" data-toggle="tooltip" title="Delete participant" class="btn btn-effect-ripple btn-xs btn-danger"><i class="fa fa-times"></i></a>
                         </td>
                     </tr>
+
                     <?php } ?>
                    
                 </tbody>
               
             </table>
+             <center><a href="addParticipant.php?idmk=<?php echo $idmk; ?>"><button type="submit" class="btn btn-effect-ripple btn-primary" name="submit" id="submit">add Participant</button></a></center>
         </div>
     </div>
     <!-- END Datatables Block -->        
